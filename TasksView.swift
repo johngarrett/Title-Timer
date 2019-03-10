27125a8cd01992885a2503ef5c67c09f6bae76dd
John Garrett <jacgarrett18@gmail.com>
2019-03-10 16:52:19 -0400

    Skeleton for tasks table view

---
+++ TasksView.swift
@@ -0,0 +1,16 @@
+//
+//  TasksView.swift
+//  Title Timer
+//
+//  Created by John Garrett on 3/10/19.
+//  Copyright © 2019 John Garrett. All rights reserved.
+//
+
+import Cocoa
+
+class TasksView: NSScrollView, NSTableViewDelegate, NSTableViewDataSource {
+	//ps -eo pid,state,start,etime,,command
+	// /bin/ps
+	
+	@IBOutlet var tableView: NSTableView!
+}
