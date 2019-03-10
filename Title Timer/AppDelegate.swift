ac3d9b00c81a2ba60692d75b613aefe35ae0cbeb
John Garrett <jacgarrett18@gmail.com>
2019-03-09 20:04:52 -0500

    Initial Commit

---
+++ Title Timer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Title Timer
+//
+//  Created by John Garrett on 3/9/19.
+//  Copyright © 2019 John Garrett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
