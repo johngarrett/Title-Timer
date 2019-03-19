2de4047c27b406df243f3fce6f7d575fb809ad3b
John Garrett <jacgarrett18@gmail.com>
2019-03-19 17:11:58 -0400

    Table view population

--- TasksView.swift
+++ TasksView.swift
@@ -14,14 +14,15 @@ class TasksView: NSScrollView, NSTableViewDelegate, NSTableViewDataSource {
 	@IBOutlet var tableView: NSTableView!
 	var tasks = [Substring]()
     var uptime = [Substring]()
+    
 	func loadTasks(){
 		let task = Process()
-		task.launchPath = "/bin/ps"
-        let username = NSUserName()
-        task.arguments = ["-eo time, command", "-u \(username)"]
+		task.launchPath = "/bin/ps" //the location of the ps command
+        task.arguments = ["-eo time, command",
+                          "-u \(NSUserName())"] //only display processes run by the user
 		let pipe = Pipe()
 		task.standardOutput = pipe
-		task.standardError = pipe
+		task.standardError  = pipe
 		task.launch()
 		task.waitUntilExit()
 		let data = pipe.fileHandleForReading.readDataToEndOfFile()
@@ -40,13 +41,14 @@ class TasksView: NSScrollView, NSTableViewDelegate, NSTableViewDataSource {
             var splitLine = line.split(separator: " ", maxSplits: 1)
             let time = splitLine[0]
             let command = splitLine.count > 1 ? splitLine[1] : "Unknown Application"
-            let path = command.split(separator: "/")
+            let path = command.split(separator: "/")//split by directories
 
             if path[0] == "Applications"{ //if the program is known to the user basically
                 let programName = path[path.count - 1] //the last item in the path e.g. /var/apps/name -> name
                                  .split(separator: "-") //dont read the flags
                 
                 uptime.append(time)
+                print(programName[0])
                 tasks.append(programName[0])
             }
         }
@@ -54,10 +56,26 @@ class TasksView: NSScrollView, NSTableViewDelegate, NSTableViewDataSource {
 	func numberOfRows(in tableView: NSTableView) -> Int {
 		return tasks.count
 	}
+
 	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-		if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier.init("TimerCell"), owner: nil) as? TimerCell {
-			return cell
-		}
-		return nil
-	}
+        var text = ""
+        var id = ""
+        
+        switch tableColumn{
+        case tableView.tableColumns[0]:
+            text = String(tasks[row])
+            id = "taskCell"
+        case tableView.tableColumns[1]:
+            text = String(uptime[row])
+            id = "timeCell"
+        default:
+            print("Error assigning value")
+        }
+        
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: id), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
 }
