7053c06e8da9274f09a967b0f523bbbe1ee73da7
John Garrett <jacgarrett18@gmail.com>
2019-03-21 02:24:05 -0400

    more merging

--- Title Timer/TimerView.swift
+++ Title Timer/TimerView.swift
@@ -20,36 +20,37 @@ class TimerView: NSScrollView, NSTableViewDelegate, NSTableViewDataSource, Timer
 	func numberOfRows(in tableView: NSTableView) -> Int {
 		return timers.count
 	}
-	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-		if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier.init("TimerCell"), owner: nil) as? TimerCell {
-			cell.titleTextField.stringValue = timers[row]
-			cell.elapsedTime = UserDefaults.standard.double(forKey: "\(timers[row])_TIME_ELAPSED")
-			cell.time        = UserDefaults.standard.string(forKey: "\(timers[row])_TIME") ?? "0 seconds"
-			cell.actionButton.image = NSImage(named: "Play")
-			cell.actionButton.image?.size = NSSize(width: 16.5, height: 16.5)
-			cell.delegate = self
-			return cell
-		}
-		return nil
-	}
-	
-	func removeCell(withString string: String){
-		guard let index = timers.firstIndex(of: string) else { return }
-		let deletionIndex = IndexSet(integer: index)
-		tableView.removeRows(at: deletionIndex, withAnimation: .slideLeft)
-		timers.remove(at: index)
-		let defaults = UserDefaults.standard
-		defaults.set(timers, forKey: "userTimers")
-		delegate?.calculateHeight()
-	}
-	//pause the timers causing the times to be saved
-	func saveValues(completion: ()-> Void){
-		guard timers.count > 0 else { completion(); return }
-		for i in 0...timers.count - 1 {
-			if let cell = tableView.view(atColumn: 0, row: i, makeIfNecessary: false) as? TimerCell{
-				cell.pauseTimer()
-			}
-		}
-		completion()
-	}
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier.init("TimerCell"), owner: nil) as? TimerCell {
+            cell.titleTextField.stringValue = timers[row]
+            cell.elapsedTime = UserDefaults.standard.double(forKey: "\(timers[row])_TIME_ELAPSED")
+            cell.time        = UserDefaults.standard.string(forKey: "\(timers[row])_TIME") ?? "0 seconds"
+            cell.actionButton.image = NSImage(named: "Play")
+            cell.actionButton.image?.size = NSSize(width: 16.5, height: 16.5)
+            cell.delegate = self
+            return cell
+        }
+        return nil
+    }
+    
+    func removeCell(withString string: String){
+        guard let index = timers.firstIndex(of: string) else { return }
+        let deletionIndex = IndexSet(integer: index)
+        tableView.removeRows(at: deletionIndex, withAnimation: .slideLeft)
+        timers.remove(at: index)
+        let defaults = UserDefaults.standard
+        defaults.set(timers, forKey: "userTimers")
+        delegate?.calculateHeight()
+    }
+    //pause the timers causing the times to be saved
+    func saveValues(completion: ()-> Void){
+        guard timers.count > 0 else { completion(); return }
+        for i in 0...timers.count - 1 {
+            if let cell = tableView.view(atColumn: 0, row: i, makeIfNecessary: false) as? TimerCell{
+                cell.pauseTimer()
+            }
+        }
+        completion()
+    }
 }
+
